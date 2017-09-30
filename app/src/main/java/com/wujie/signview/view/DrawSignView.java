cad9c7a0bf065155c2e5c9b3d0058ae03a34d8be
wujie <1072878103@qq.com>
2017-09-30 11:22:08 +0800

    添加上米字格的功能

--- app/src/main/java/com/wujie/signview/view/DrawSignView.java
+++ app/src/main/java/com/wujie/signview/view/DrawSignView.java
@@ -4,12 +4,15 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.DashPathEffect;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathEffect;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Handler;
 import android.os.Message;
-import android.util.AttributeSet;
 import android.view.View;
 
 import com.wujie.signview.R;
@@ -29,13 +32,129 @@ public class DrawSignView {
      */
     public class CanvasView extends View {
 
-        public CanvasView(Context context) {
-            super(context);
+        private Path mPath; // 米字格中的路径
+
+        private android.graphics.Path mmPath; // 横线上的路径按比例缩放
+
+        private CanvasPath myPath;// 记录路径的常数
+
+        private PathListener pathListener; //完成一次书写的回调
+
+        private Paint mPaint;
+
+        private int narrowNum = 10;// 缩小的倍数
+
+        private int width;
+
+        private Timer timer = null;
+
+        private TimerTask task = null;
+
+
+
+        private float TOUCH_TOLERANCE = 4; // 画的平滑程度 设置的越大越粗略
+
+        private float mX, mY; //点击时坐标的及时位置
+
+        private float maxX; //写字时 x方向的最大位移
+        private float minX;
+
+        private float maxY;
+        private float minY;
+
+        private float ddx;
+        private float ddy;
+
+        private float cavasViewSize;
+
+        private Bitmap bitmap;
+        private Canvas canvas;
+
+        public PathListener getPathListener() {
+            return pathListener;
         }
 
-        public CanvasView(Context context, AttributeSet attrs) {
-            super(context, attrs);
+        public void setPathListener(PathListener pathListener) {
+            this.pathListener = pathListener;
         }
+
+        public Paint getmPaint() {
+            return mPaint;
+        }
+
+        public void setmPaint(Paint mPaint) {
+            this.mPaint = mPaint;
+        }
+
+        public int getNarrowNum() {
+            return narrowNum;
+        }
+
+        public void setNarrowNum(int narrowNum) {
+            this.narrowNum = narrowNum;
+        }
+
+        private boolean isFirst = true;
+
+        final Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+            }
+        };
+
+        public CanvasView(Context c , Paint paint, int width) {
+            super(c);
+            this.width = width;
+            mPath = new Path();
+            mmPath = new Path();
+            this.mPaint = paint;
+            TOUCH_TOLERANCE = dip2px(c, TOUCH_TOLERANCE);
+            drawHandlerWritingBox();
+            setBackgroundDrawable(new BitmapDrawable(bitmap));
+
+        }
+
+        private void drawHandlerWritingBox() {
+
+            if (bitmap != null) {
+                bitmap.recycle();
+            }
+
+            cavasViewSize = width;
+            float t = mPaint.getStrokeWidth();
+
+            bitmap = Bitmap.createBitmap((int) cavasViewSize,
+                    (int) cavasViewSize, Bitmap.Config.ARGB_4444);
+            canvas = new Canvas(bitmap);
+
+            PathEffect effect = new DashPathEffect(new float[] {5,5,5,5}, 1);
+
+            mPaint.setStrokeWidth(1);
+            mPaint.setPathEffect(effect);
+            canvas.drawLine(0, 0, cavasViewSize, cavasViewSize, mPaint);
+            canvas.drawLine(0, cavasViewSize, cavasViewSize, cavasViewSize,
+                    mPaint);
+            canvas.drawLine(0, 0, cavasViewSize, 0, mPaint);
+            canvas.drawLine(cavasViewSize, 0, 0, cavasViewSize, mPaint);
+            canvas.drawLine(1, 0, 1, cavasViewSize, mPaint);
+            canvas.drawLine(cavasViewSize - 1, 0, cavasViewSize - 1,
+                    cavasViewSize, mPaint);
+            canvas.drawLine(0, cavasViewSize / 2, cavasViewSize,
+                    cavasViewSize / 2, mPaint);
+            canvas.drawLine(cavasViewSize / 2, 0, cavasViewSize / 2,
+                    cavasViewSize, mPaint);
+
+            mPaint.setPathEffect(null);
+            mPaint.setStrokeWidth(t);
+
+        }
+
+    }
+
+    public interface  PathListener{
+
+        public void onPath(CanvasPath myPath);
     }
 
 
@@ -191,6 +310,102 @@ public class DrawSignView {
     }
 
     /**
+     * 用来记录路径的实体类
+     */
+    public class CanvasPath {
+        public static final int C_iCanVasType_space = 1;
+        public static final int C_iCanVasType_enter = 2;
+        public static final int C_icanVasType_handleWriting = 3;
+
+        private Path path;
+        private Bitmap bitmap;
+
+        private float maxX;
+        private float maxY;
+
+        private float startX;
+        private float startY;
+
+        private float endX;
+        private float endY;
+
+        private int canvasType = C_icanVasType_handleWriting;
+
+        public Path getPath() {
+            return path;
+        }
+
+        public void setPath(Path path) {
+            this.path = path;
+        }
+
+        public Bitmap getBitmap() {
+            return bitmap;
+        }
+
+        public void setBitmap(Bitmap bitmap) {
+            this.bitmap = bitmap;
+        }
+
+        public float getMaxX() {
+            return maxX;
+        }
+
+        public void setMaxX(float maxX) {
+            this.maxX = maxX;
+        }
+
+        public float getMaxY() {
+            return maxY;
+        }
+
+        public void setMaxY(float maxY) {
+            this.maxY = maxY;
+        }
+
+        public float getStartX() {
+            return startX;
+        }
+
+        public void setStartX(float startX) {
+            this.startX = startX;
+        }
+
+        public float getStartY() {
+            return startY;
+        }
+
+        public void setStartY(float startY) {
+            this.startY = startY;
+        }
+
+        public float getEndX() {
+            return endX;
+        }
+
+        public void setEndX(float endX) {
+            this.endX = endX;
+        }
+
+        public float getEndY() {
+            return endY;
+        }
+
+        public void setEndY(float endY) {
+            this.endY = endY;
+        }
+
+        public int getCanvasType() {
+            return canvasType;
+        }
+
+        public void setCanvasType(int canvasType) {
+            this.canvasType = canvasType;
+        }
+
+    }
+
+    /**
      * 光标的参数类
      */
     public class CursorPar {
@@ -265,5 +480,11 @@ public class DrawSignView {
         private int cursorColor = Color.BLACK;
 
     }
+
+    public int dip2px(Context c,float px) {
+        float de = c.getResources().getDisplayMetrics().density;
+        return (int) (px * de + 0.5f);
+    }
+
 
 }

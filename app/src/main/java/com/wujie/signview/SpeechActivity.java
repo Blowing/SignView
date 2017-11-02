29c4283c3d32f03674a22658245b3b89d8981e95
wujie <1072878103@qq.com>
2017-11-02 11:07:10 +0800

    1 提交初始界面

---
+++ app/src/main/java/com/wujie/signview/SpeechActivity.java
@@ -0,0 +1,13 @@
+package com.wujie.signview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpeechActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech);
+    }
+}
